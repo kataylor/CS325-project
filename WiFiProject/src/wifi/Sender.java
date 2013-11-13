dc8f2292eedd5d8ae4fbf189a117d659ce19d6ad
Christopher Livingston <ChristopherLivingston92@gmail.com>
2013-11-13 13:14:19 -0800

    Added comments

--- WiFiProject/src/wifi/Sender.java
+++ WiFiProject/src/wifi/Sender.java
@@ -2,21 +2,43 @@ package wifi;
 
 import rf.RF;
 
+/*
+ * Takes the frame and transmits the data through the RF layer.
+ * 
+ * @implements Runnable
+ */
+
 public class Sender implements Runnable {
 	RF theRF;
 	byte[] data;
 
+	/*
+	 * Constructor. Takes an RF layer and the frame to be transmitted.
+	 */
 	public Sender(RF theRF, byte[] data) {
-		theRF = this.theRF;
-		data = this.data;
+		this.theRF = theRF;
+		this.data = data;
 	}
 
+	/*
+	 * Needed to implement Runnable.
+	 * 
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
 	public void run() {
 		boolean sent = false;
 		while (sent == false) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// Do nothing
+			}
 			if (theRF.inUse() != true) {// if no one is currently transmitting
 				int bytesSent = theRF.transmit(data);
-				if (bytesSent == data.length) {// if we sent the correct number of bytes.
+				if (bytesSent == data.length) {// if we sent the correct number
+												// of bytes.
 					System.out.println("We sent the packet!");
 					// kill the thread here
 					sent = true;
